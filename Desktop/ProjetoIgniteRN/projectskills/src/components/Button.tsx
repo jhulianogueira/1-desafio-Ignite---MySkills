7b9a74e0099465c2fb78f2e3dc490d839b9477d6
Jhulia Nogueira <jhulia.nogueira@pier.digital>
2021-12-28 18:11:56 -0400

    first commit

---
+++ Desktop/ProjetoIgniteRN/projectskills/src/components/Button.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {TouchableOpacity, Text, StyleSheet, TouchableOpacityProps} from 'react-native';
+
+
+interface ButtonProps extends TouchableOpacityProps{
+  title: string;
+}
+
+export function Button({title, ...rest} : ButtonProps) {
+  return (
+    <TouchableOpacity 
+      style={styles.button} 
+      {...rest}
+    >
+        <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: '#d83bd2',
+    padding: 15,
+    borderRadius: 7,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 17,
+    fontWeight: 'bold',
+  },
+});
